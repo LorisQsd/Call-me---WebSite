ec6455e27b4fd89cd477516df41ee3f3f4af8987
Loris QUESADO <loris.quesado@oclock.school>
2023-11-14 13:14:15 +0100

    Navbar - Mobile with typescript toggle feature

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log("coucou")
+// NAVBAR
+let isNavbarOpen = false;
+
+const navbarBtn = document.getElementById('navbarBtn');
+const navLinks = document.querySelectorAll('#navlink');
+
+const handleNavbar = () => {
+  // Elements
+  const firstBar = document.getElementById('firstBar');
+  const secondBar = document.getElementById('secondBar');
+  const thirdBar = document.getElementById('thirdBar');
+
+  const headerElement = document.getElementById('header');
+
+  // Toggler
+  headerElement?.classList.toggle('translate-x-0');
+  isNavbarOpen = !isNavbarOpen;
+  
+  firstBar?.classList.toggle("rotate-45")
+  firstBar?.classList.toggle("translate-y-[9px]")
+  secondBar?.classList.toggle("opacity-0")
+  thirdBar?.classList.toggle("-rotate-45")
+  thirdBar?.classList.toggle("-translate-y-[10px]")
+}
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", handleNavbar)
+})
+
+navbarBtn?.addEventListener('click', handleNavbar);
