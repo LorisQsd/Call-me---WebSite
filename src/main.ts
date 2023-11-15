9d2b3966ad60a9fd653fcb291cd80b6ab8545e35
Loris QUESADO <loris.quesado@oclock.school>
2023-11-15 10:00:50 +0100

    Contact Form - Mobile

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ const handleNavbar = () => {
   isNavbarOpen = !isNavbarOpen;
   
   firstBar?.classList.toggle("rotate-45")
-  firstBar?.classList.toggle("translate-y-[9px]")
+  firstBar?.classList.toggle("translate-y-[10px]")
   secondBar?.classList.toggle("opacity-0")
   thirdBar?.classList.toggle("-rotate-45")
   thirdBar?.classList.toggle("-translate-y-[9px]")
@@ -28,3 +28,17 @@ navLinks.forEach((link) => {
 })
 
 navbarBtn?.addEventListener('click', handleNavbar);
+
+// === CONTACT FORM === //
+const contactForm = document.getElementById("contactForm");
+
+const handleSubmit = (e: any) => {
+  e.preventDefault();
+  
+  const formData = new FormData(e.currentTarget);
+  const formEntries = Object.fromEntries(formData);
+
+  console.log(formEntries)
+}
+
+contactForm?.addEventListener("submit", handleSubmit)
