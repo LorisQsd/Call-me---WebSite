b0521f73d511c6b77265b8d672914cee92967256
Loris QUESADO <loris.quesado@oclock.school>
2023-11-15 15:41:31 +0100

    Observer & scroll animations

--- src/observer.ts
+++ src/observer.ts
@@ -1,29 +1,59 @@
 /* eslint-disable no-restricted-syntax */
 const headerElement = document.getElementById('header');
+const navlinks = headerElement?.querySelectorAll('#navlink');
+
+// SECTIONS
 const homeSection = document.getElementById('home');
 const featureSection = document.getElementById('feature');
+const descriptionSection = document.getElementById('description');
+const autonomySection = document.getElementById('autonomy');
+const usecaseSection = document.getElementById('usecase');
+const contactSection = document.getElementById('contact');
 
 // === OBSERVER === //
 const observer = new IntersectionObserver(
   (entries) => {
     for (const entry of entries) {
-      // Si je ne suis pas sur la section home, je cache la navbar
+      // If we don't observe Home Section, we want to hide the navbar
       if (
-        entry.rootBounds && 
+        entry.rootBounds &&
         entry.rootBounds.width > 1024 &&
         entry.target === homeSection
       ) {
-        console.log("pouet")
         if (!entry.isIntersecting)
           headerElement?.classList.add('-translate-y-full');
         else headerElement?.classList.remove('-translate-y-full');
       }
 
-      console.log(entry);
+      if (entry.isIntersecting) {
+        // If we observe any section, we want to change the style of the mobile navbar
+        const sectionId = entry.target.getAttribute('id');
+
+        navlinks?.forEach((navlink) => {
+          const hrefAttr = navlink.getAttribute('href')?.slice(1);
+
+          if (sectionId === hrefAttr && sectionId !== 'contact') {
+            navlink.classList.add('text-primary-400');
+            navlink.classList.add('font-semibold');
+          } else {
+            navlink.classList.remove('text-primary-400');
+            navlink.classList.remove('font-semibold');
+          }
+        });        
+        
+      } 
+
+      if(entry.isIntersecting && entry.target !== homeSection) {
+        entry.target.classList.add("slide-fade-up__animate")
+      }
     }
   },
-  { rootMargin: '-100px 0px 0px 0px' }
+  { rootMargin: '-100px 0px -200px 0px' }
 );
 
 observer.observe(homeSection as HTMLElement);
 observer.observe(featureSection as HTMLElement);
+observer.observe(descriptionSection as HTMLElement);
+observer.observe(autonomySection as HTMLElement);
+observer.observe(usecaseSection as HTMLElement);
+observer.observe(contactSection as HTMLElement);
